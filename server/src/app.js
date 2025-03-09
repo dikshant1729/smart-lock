f6e4da9a5797372d5f116f92415fcf3a3e4cf4f6
Piyush@laptop <chandra7piyush@gmail.com>
2025-03-09 23:02:24 +0530

    project planning

--- server/src/app.js
+++ server/src/app.js
@@ -12,5 +12,55 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", googleAuth);
+app.use("/lock");
+
+// /get -> all the locks present for the email
+// /post -> add a lock {pairing} => check if email != null (already paired)
+// /password -> set password {pairing code, password}
+// /delete -> delete lock (email => null)
+
+/*
+{
+    id: "",
+    password : null,
+    active: true,
+    email: null
+}
+*/
+
+//socket with lock
+// on socket connection => store id on db
+// on user gives pairing code => match db
+// on matching, no password found => matching starts else already paired
+
+// lock pairing
+/*
+- first user gives pairing code
+- check if pairing exist or device is already paired
+- password set
+- password => encrypt => db
+*/
+
+// on user delete => password == null
+//
+
+/*
+- login => :)
+- 1 user -> multiple lock
+- user -> email
+- pair (socket id) -> password set
+- socket -> password enter => script run => data to ml model => sucess/failure
+*/
+
+//lock open
+/*
+- socket connection 
+- on request => script run 
+- show sucess and failure
+*/
+
+//4 routes
+// -> 2 route for lock pairing
+// -> socket connection
 
 module.exports = app;
