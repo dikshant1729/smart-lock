8f359d5e0a0861bc6aa82aa1bfcb0fd0fe45d8d7
Piyush@laptop <chandra7piyush@gmail.com>
2025-03-11 01:34:11 +0530

    Socket initialized

--- server/src/config/SocketController.js
+++ server/src/config/SocketController.js
@@ -1,6 +1,8 @@
 const { Server } = require("socket.io");
+const lockModel = require("../models/lock.model");
 
 var io;
+const activeUsers = new Map();
 
 const initSocket = (server) => {
     io = new Server(server, {
@@ -12,8 +14,47 @@ const initSocket = (server) => {
 
     io.on("connect", (socket) => {
         console.log("New Socket Connection", socket.id);
+        var _code;
 
-        socket.on("message", () => {}); //
+        socket.on("event:init", async (code) => {
+            if (activeUsers.get(code)) {
+                io.emit("event:regen_code");
+            } else {
+                activeUsers.set(code, socket.id);
+                const lock = await lockModel.findOne({ pairingCode: code });
+                if (!lock) {
+                    await lockModel.create({ pairingCode: code });
+                } else {
+                    await lockModel.findOneAndUpdate(
+                        { pairingCode: code },
+                        { active: true }
+                    );
+                }
+                _code = code;
+            }
+        });
+
+        var clientData;
+
+        socket.on("event:open", (code, data) => {
+            if(activeUsers.get(code)===socket.id){
+                clientData = data;
+                io.emit("event:run");
+            }
+        });
+
+        socket.on("event:match", (data)=> {
+            //ML model run
+        })
+
+        socket.on("disconnect", async () => {
+            activeUsers.delete(_code);
+            await lockModel.findOneAndUpdate(
+                { pairingCode: _code },
+                { active: false }
+            );
+            console.log(`User disconnected: ${socket.id}`);
+        });
     });
 };
 
--- server/src/index.js
+++ server/src/index.js
@@ -6,8 +6,8 @@ const { initSocket } = require("./config/SocketController");
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
-initSocket(server);
 connectToDb();
+initSocket(server);
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
